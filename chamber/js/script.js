d5a650c3e12a99d73361b0c0cd383f92d3d8ef8a
Chatayika <cchatayika@outlook.com>
2022-01-30 02:07:31 +0200

    commit

---
+++ chamber/js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+document.getElementId(primaryNav),classlist.toggle(open)
+document.getElementId(hamburgerBtn),classlist.toggle(open)
+}
+
+const x = docement.getElementId(hamburgerBtn)
+
+x.onclick=  toggleMenu
