f4c2e704e4bc094da94fe184bc154751edcc6450
Chatayika <cchatayika@outlook.com>
2022-02-06 08:35:14 +0200

    new commit

---
+++ chamber/script.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector("#hamburger")
+const nav = document.querySelector("nav ul") 
+
+hamburger.addEventListener("click", toggleMenu)
+
+hamburger.onclick=function toggleMenu(){
+    document.getElementsByClassName("nav")[0].classList.toggle("responsive")
+    document.getElementById("#hamburger").style.display. visibility=hidden;
+    document.getElementById("#hamburger").style.display. position=absolute; 
+    document.getElementById("#hamburger").style.display. left=0;
+    document.getElementById("#hamburger").style.display. top=0; 
+    document.getElementById("#hamburger").style.display. content= "❌ " ; 
+}
+date= new Date()
+let dayOfWeek= date.getDay()
+
+if (dayOfWeek==1 && dayOfWeek == 2){
+    document.createElement("div")=banner
+    banner.textContent="🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
+    header.appendChild(banner) 
+}
+else{ document.createElement("div")=""}
+
+
+
