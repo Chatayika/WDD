aa2860dfc09338d60a9d5070f0b0182976466488
Chatayika <cchatayika@outlook.com>
2022-02-13 04:00:56 +0200

    correction

--- book/bom.js
+++ book/bom.js
@@ -1,27 +1,23 @@
-const list = document.querySelector('.list');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+const input = document.querySelector("#favorateChapter");
+const button = document.querySelector(".button1");
+const list = document.querySelector(".list");
 
-let myInput= input.value;
+button.addEventListener("click", function () {
+    if (input && input.value) {
+            const item = list.appendChild(Object.assign(document.createElement("li"), {
+                class: "list-item",
+                innerHTML: input.value
+            }))
 
-button.addEventListener('click',function(){
+            item.appendChild(Object.assign(document.createElement("button"), {
+                class: "button2",
+                type: "submit",
+                innerHTML: "❌"
+            })).addEventListener("click", function () {
+                list.removeChild(item);
+            })
+            input.focus();
+            input.value = "";
 
-    const chapter=document.createElement('li');
-    const chapterButton=document.createElement('button');
-    chapter.textContent=myInput;
-    chapterButton.textContent='X';
-
-    chapter.appendChild(chapterButton);
-    list.appendChild(chapter);
-
-    chapterButton.onclick = function(e) {
-       list.removeChild(chapter);};
-
-    input.focus();
-
-    let myInput="";
-
-    input.focus();
-    
+    }
 })
-
