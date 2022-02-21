2d50eace96454e789eef5111408c45f8a0772934
Chatayika <cchatayika@outlook.com>
2022-02-21 03:47:50 +0200

    commit

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -6,11 +6,12 @@ hamburger.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
-date= new Date();
-const header = document.querySelector("header")
+
+date= new Date()
+const header = document.querySelector("h1")
 const para = document.createElement("p")
 para.innerHTML=date
-header.appendChild(para)
+header.appendChild(para).style.fontSize="14px"
 
 
 
@@ -20,10 +21,12 @@ let dayOfWeek= date.getDay();
 if (dayOfWeek!=1|dayOfWeek != 2){
     const ban = document.querySelector("banner")
     ban.style.display="none"
+
+
 }
-else{
-    ban.style.display = "block"
-}
+//else{
+//    ban.style.display = "block"
+
 
 
 
